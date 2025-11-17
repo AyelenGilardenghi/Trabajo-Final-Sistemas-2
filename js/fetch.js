15bc28cfeaddd4a00f2dc4b1881715c73ce0796c
Ayelen Gilardenghi <ayelenyasmin@gmail.com>
2025-11-17 17:04:01 -0300

    poner imagen a la solapa incio

--- js/fetch.js
+++ js/fetch.js
@@ -1,39 +1,38 @@
 const $personajesList = document.getElementById("personajesList");
 
 fetch("https://hp-api.onrender.com/api/characters")
-  .then((respuesta) =>{
-    console.log(respuesta);
-   return respuesta.json();
-  })
-  .then((data) => {
-    
-    const personajes = data;
-  //  console.log(personajes);
+  .then(res => res.json())
+  .then(personajes => {
 
-    personajes.forEach(
-      (personaje) => {
-        var fechaNacimiento;
+    let html = ""; // acumulamos todo acá
 
-        if(personaje.dateOfBirth === null)
-          fechaNacimiento = "Sin Fecha";
-        else
-          fechaNacimiento = personaje.dateOfBirth;
-
-        var fechaNacimiento = personaje.dateOfBirth === null?"sin fecha":personaje.dateOfBirth;
-
-        $personajesList.innerHTML += 
-        `<div class="grid-item bordes_redondeados  house_${personaje.house}">
-        <h2 class="h2_grid" ><img class='logo_house' src='../img/houses/${(personaje.house)?personaje.house:'sin_casa'}.png' alt="Casa" /><span>${personaje.name}</span></h2>
+    personajes.forEach(personaje => {
       
-        <div>
-        <img class="imagen_personaje bordes_redondeados" src="${(personaje.image)?personaje.image:'../img/sin_imagen.png'}" alt="Imagen Personaje" />
-        <div class="atributos"> 
-          <div> Especie: ${personaje.species}</div>
-          <div>Género: ${personaje.gender}</div>
-         <div>Nacimiento: ${fechaNacimiento} </div>
-        <div>Casa: ${personaje.house}</div>`;
+      let fechaNacimiento = personaje.dateOfBirth 
+        ? personaje.dateOfBirth 
+        : "Sin fecha";
 
-      }
-    );
-  
+      html += `
+        <div class="grid-item bordes_redondeados house_${personaje.house}">
+          <h2 class="h2_grid">
+            <img class="logo_house" src="../img/houses/${personaje.house ? personaje.house : 'sin_casa'}.png" alt="Casa" />
+            <span>${personaje.name}</span>
+          </h2>
+
+          <div>
+            <img class="imagen_personaje bordes_redondeados" src="${personaje.image ? personaje.image : '../img/sin_imagen.png'}" alt="Imagen Personaje" />
+            
+            <div class="atributos"> 
+              <div>Especie: ${personaje.species}</div>
+              <div>Género: ${personaje.gender}</div>
+              <div>Nacimiento: ${fechaNacimiento}</div>
+              <div>Casa: ${personaje.house}</div>
+            </div>
+          </div>
+        </div>
+      `;
+    });
+
+    // inserción UNA sola vez
+    $personajesList.innerHTML = html; 
   });
